8d9ace9a3ec6506710d505726a06378163b37e76
tkm3845 <takumi3845@gmail.com>
2022-12-07 16:17:44 +0900

    ACOTableをmultiset方式にした

--- src/ACOTable.cpp
+++ src/ACOTable.cpp
@@ -2,24 +2,23 @@
 
 ACOTable::ACOTable(){
   //NOTE: 時間経過してもフェロモンの順序関係は変わらないのでこれで良い
-  priority_queue([this](Ant &a,Ant &b){return a.pheromone(time)>b.pheromone(time);});
+  multiset([this](Ant &a,Ant &b){return a.pheromone(time)>b.pheromone(time);});
 }
 Ant ACOTable::selectAnt(){
   double sum=0;
-  for(int i=0;i<this->c.size();i++)sum+=this->c[i].pheromone(this->time);
   //TODO: 実装!
 }
 double ACOTable::costVariance(){
   double sum=0,squareSum=0,temp;
-  for(int i=0;i<this->c.size();i++){
-    temp=this->c[i].allCost();
+  for(Ant a:*this){
+    temp=a.allCost();
     sum+=temp;squareSum+=temp*temp;
   }
-  return (squareSum-sum)/this->c.size();
+  return (squareSum-sum)/this->size();
 }
-void ACOTable::push(const Ant &a){
-  priority_queue::push(a);
-  while(this->size()>this->maxSize)this->pop();
+void ACOTable::insert(const Ant &a){
+  multiset::insert(a);
+  if(this->size()>this->maxSize)this->erase(this->begin());
 }
 void ACOTable::setTime(ll time){this->time=time;}
 void ACOTable::setTableSize(ll size){this->maxSize=size;}
--- src/ACOTable.hpp
+++ src/ACOTable.hpp
@@ -1,15 +1,14 @@
 #ifndef _ACO_TABLE_HPP_
 #define _ACO_TABLE_HPP_
 #include<array>
-#include<queue>
+#include<set>
 #include<functional>
 #include"Ant.hpp"
 using namespace std;
 using ll=long long;
 
-class ACOTable : public priority_queue<Ant,vector<Ant>,
-                                       function<bool(Ant&,
-                                       Ant&)>> {
+//最良と最悪どちらも取り出したいのでmultiset方式
+class ACOTable : public multiset<Ant,function<bool(Ant&,Ant&)>> {
   private:
     ll time=0,maxSize=16;
   public:
@@ -18,7 +17,7 @@ class ACOTable : public priority_queue<Ant,vector<Ant>,
     void setTime(ll time);
     void setTableSize(ll size);
     double costVariance();
-    void push(const Ant &a);
+    void insert(const Ant &a);
 };
 
 
