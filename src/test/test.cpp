fd341eb323e35f8ee09c7fc932a0a1fd33a3b611
tkm3845 <takumi3845@gmail.com>
2022-12-25 15:10:53 +0900

    別関数のローカルインスタンスを引数に保存した時の動作を確認した

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+#include<vector>
+#include"../main/QuadTree.hpp"
 using namespace std;
 
+void push(vector<vector<int>> &v,int begin){
+  vector<int>tmp;
+  for(int i=0;i<10;i++)tmp.push_back(i+begin);
+  v.push_back(tmp);
+}
+
 int main(void){
-  printf("hoge\n");
+  vector<vector<int>>v;
+  for(int i=0;i<5;i++)push(v,i);
+  for(int i=0;i<v.size();i++)
+    for(int j=0;j<v[i].size();j++)
+      cout<<v[i][j]<<(j!=v[i].size()-1?' ':'\n');
+  return 0;
 }
