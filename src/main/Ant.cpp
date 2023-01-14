db116e2b13a1af44ec7d2c6f97f988db20d37eb2
tkm3845 <takumi3845@gmail.com>
2023-01-15 00:49:34 +0900

    Antで、コストと長さの計算とAnt::tracePushBack()を追加した

--- src/main/Ant.cpp
+++ src/main/Ant.cpp
@@ -14,7 +14,7 @@ struct Joint{
 
 struct SingleRoute{
   Joint joint;
-  vector<v2d> route;
+  vector<v2d> points;
   double cost=0,length=0;
 };
 
@@ -33,7 +33,7 @@ Ant::Ant(const ACOSteiner &world) : BIRTH_TIME(world.getTime()),
 }
 
 inline const vector<v2d>& Ant::getRelayPointsOnRoute(int index) const {
-  return this->path[index]->route;
+  return this->path[index]->points;
 }
 
 inline int Ant::numOfPoints() const {return this->path.size();}
@@ -46,6 +46,8 @@ inline double Ant::pheromone() const {return this->pheromone_v;}
 
 static random_device seed_gen;
 static default_random_engine random_engine(seed_gen());
+static uniform_real_distribution<> uniform_unit_dist(0,1);
+static normal_distribution<> norm_unit_dist(0,1);
 
 void Ant::constructFirstRoute(const ACOSteiner &world){
   //TODO: 初期の蟻の実装!
@@ -61,6 +63,7 @@ void Ant::constructModifiedRoute(const ACOSteiner &world,ll current_time){
     sum+=e->pheromone();
     if(sum>=rand){base_ant=e;break;}
   }
+  const int target_route_index=random_engine()%this->path.size();
   if(this->MIN_DISTANCE==base_ant->MIN_DISTANCE&&this->MAX_DISTANCE==base_ant->MAX_DISTANCE){
     for(int i=0;i<this->path.size();i++)this->path[i]=base_ant->path[i];
   }else{
@@ -68,7 +71,6 @@ void Ant::constructModifiedRoute(const ACOSteiner &world,ll current_time){
       //TODO: 間隔と接合点を考慮しながら経路をコピーするコードを書く
     }
   }
-  int target_route_index=random_engine()%this->path.size();
   addRandVecToOneRoute(world,base_ant,target_route_index);
 }
 
@@ -78,20 +80,17 @@ static double moveStandardDeviation(double distance_c2j,double basic_move_ratio,
   return distance_c2j*basic_move_ratio*min(1.0,10*stdev_cost/mean_length+mutation);
 }
 
-void Ant::addRandVecToOneRoute(const ACOSteiner &world, const Ant *base_ant,
+enum class JointTarget{
+  ANOTHER_ANT,
+  ANOTHER_OWN_ROUTE
+};
+
+JointTarget Ant::addRandVecToOneRoute(const ACOSteiner &world, const Ant *base_ant,
                                const int target_index) {
   this->path[target_index].reset(new SingleRoute);
   const ACOTable &TABLE=world.getACOTable();
-  const QuadTreeAnt &QTA=world.getQuadTreeAnt(target_index);
-  const auto &START_POINT=world.getConnectPoint(target_index);
-  const auto &BASE_ROUTE=base_ant->path[target_index]->route;
-  auto &target_route=this->path[target_index]->route;
-  QuadTree<const int> own_qt(QTA.width(),QTA.height());
-  for(int i=0;i<this->path.size();i++)
-    if(i!=target_index&&this->path[i]->joint.route_index!=target_index)
-      own_qt.addRoute(this->path[i]->route,i);
+  const auto &BASE_ROUTE=base_ant->path[target_index]->points;
   
-  uniform_real_distribution<> uniform_unit_dist(0,1);
   //標準偏差を掛けると発散する危険があるので以下のようにする。
   const double move_standard_deviation=moveStandardDeviation(
     euclid(BASE_ROUTE[0],BASE_ROUTE[BASE_ROUTE.size()-1]),
@@ -101,47 +100,83 @@ void Ant::addRandVecToOneRoute(const ACOSteiner &world, const Ant *base_ant,
     uniform_unit_dist(random_engine)>=world.getMutationProbability()||
       TABLE.size()<TABLE.getCapacity()
   );
-  normal_distribution<> dist_norm(0,move_standard_deviation);
-  const double randvec_norm=dist_norm(random_engine);
+  const double randvec_norm=norm_unit_dist(random_engine)*move_standard_deviation;
   array<double,2> base_random_vec{randvec_norm,0};
   rotate(base_random_vec,2*M_PI*uniform_unit_dist(random_engine));
-  const int add_main_target=random_engine()%target_route.size();
-  const int arched_add_range=(random_engine()%(target_route.size()/2))*2+1;//奇数にしたい
-  const int add_start_index=max(0,add_main_target-arched_add_range);
-  const int original_add_end_index=add_main_target+arched_add_range/2;//NOTE: 本来の中継点の個数を上回ることもある
-  const int actual_add_end_index=min((int)BASE_ROUTE.size()-1,original_add_end_index);
-  target_route.reserve(BASE_ROUTE.size());
-  v2d min_point=QTA.minPoint(),max_point=min_point+QTA.size();
-  for(int i=0;i<add_start_index;i++)
-    properPushBack(target_route,BASE_ROUTE[i],min_point,max_point);
-  //TODO: 接合点の移動を考慮する
-  for(int i=add_start_index;i<add_main_target;i++){
-    properPushBack(target_route,
-        BASE_ROUTE[i]+base_random_vec*sin(M_PI/2*i/add_main_target),
-        min_point,max_point);
-  }
-  for(int i=add_main_target;i<=actual_add_end_index;i++){
-    properPushBack(
-      target_route,
-      BASE_ROUTE[i]+base_random_vec*sin(M_PI/2*(1+(double)i/original_add_end_index)),
-      min_point,max_point
-    );
-  }
-  for(int i=actual_add_end_index+1;i<BASE_ROUTE.size();i++)
-    properPushBack(target_route,BASE_ROUTE[i],min_point,max_point);
+  archedAdd(target_index,base_ant,base_random_vec,world);
 }
 
-void Ant::properPushBack(vector<v2d> &v,const v2d &e,const v2d &min_point,const v2d &max_point){
+JointTarget Ant::archedAdd(const int target_index,const Ant *base_ant,v2d base_random_vec,const ACOSteiner &world){
+  auto &target_points=this->path[target_index]->points,
+       &base_points=base_ant->path[target_index]->points;
+  const QuadTreeAnt &QTA=world.getQuadTreeAnt(target_index);
+  QuadTree<const int> own_qt(QTA.minPoint(),QTA.size());
+  for(int i=0;i<this->path.size();i++)
+    if(i!=target_index&&this->path[i]->joint.route_index!=target_index)
+      own_qt.addRoute(this->path[i]->points,i);
+
+  const int add_main_target=random_engine()%base_points.size();
+  const int arched_add_range=(random_engine()%(base_points.size()/2))*2+1;//奇数にしたい
+  const int add_start_index=max(0,add_main_target-arched_add_range);
+  const int original_add_end_index=add_main_target+arched_add_range/2;//NOTE: 本来の中継点の個数を上回ることもある
+  const int actual_add_end_index=min((int)base_points.size()-1,original_add_end_index);
+  
+  target_points.reserve(base_points.size());
+  const double rough_total_cost=base_ant->total_cost;
+  v2d min_point=QTA.minPoint(),max_point=min_point+QTA.size();
+  auto cost_lambda=[&world](const v2d &v1,const v2d &v2){
+    return world.calcCost(v1,v2);
+  };
+  //FIXME: 範囲外に出ないことが保証されているのでproperPushBack()する意味が薄い
+  for(int i=0;i<add_start_index;i++)
+    tracePushBack(target_index,base_points[i],cost_lambda);
+  //TODO: 接合点の移動を考慮する
+  for(int i=add_start_index;i<add_main_target;i++){
+    static const int add_range=add_main_target-add_start_index;
+    properPushBack(target_index,
+        base_points[i]+base_random_vec*sin(M_PI/2*(i-add_start_index)/add_range),
+        min_point,max_point,cost_lambda);
+  }
+  for(int i=add_main_target;i<=actual_add_end_index;i++){
+    static const int add_range=original_add_end_index-add_main_target;
+    properPushBack(target_index,
+        base_points[i]+base_random_vec*
+            sin(M_PI/2*(1+(double)(i-add_main_target)/add_range)),
+        min_point,max_point,cost_lambda);
+  }
+  for(int i=actual_add_end_index+1;i<base_points.size();i++)
+    tracePushBack(target_index,base_points[i],cost_lambda);
+}
+
+void Ant::tracePushBack(const int target_index,const v2d &e,const function<double(const v2d&,const v2d&)> &cost_function){
+  SingleRoute &route=*(this->path[target_index]);
+  auto &points=route.points;
+  const v2d &last_point=points[points.size()-1];
+  points.push_back(e);
+  route.cost+=cost_function(last_point,e);
+  route.length+=euclid(last_point,e);
+}
+
+void Ant::properPushBack(const int target_index,const v2d &e,
+    const v2d &min_point,const v2d &max_point,
+    const function<double(const v2d&,const v2d&)> &cost_function){
+  SingleRoute &route=*(this->path[target_index]);
+  auto &v=route.points;
+  const v2d &last_point=v[v.size()-1];
   v2d p{max(min_point[0],e[0]),max(min_point[1],e[1])};
   p[0]=min(p[0],max_point[0]);p[1]=min(p[1],max_point[1]);
-  const double distance=euclid(v[v.size()-1],p);
-  if(distance<MIN_DISTANCE)return;//NOTE: 簡単のため、近い時は重心を取るのではなくて追加しないようにした
-  if(distance<=MAX_DISTANCE){//MIN_DISTANCE<=distanceは保証されている
-    v.push_back(p);return;
+  const double distance=euclid(last_point,p);
+  if(distance<this->MIN_DISTANCE)return;//NOTE: 簡単のため、近い時は重心を取るのではなくて追加しないようにした
+  if(distance<=this->MAX_DISTANCE){//this->MIN_DISTANCE<=distanceは保証されている
+    v.push_back(p);
+  }else{
+    const double ideal_complement_distance=mean(this->MIN_DISTANCE,this->MAX_DISTANCE);
+    const int vector_num=ceil(distance/ideal_complement_distance);//NOTE: round等だと距離条件が満たされない場合がある
+    for(int i=1;i<=vector_num;i++){
+      v.push_back((double)i/vector_num*p);
+    }
   }
-  const double ideal_complement_distance=mean(this->MIN_DISTANCE,this->MAX_DISTANCE);
-  const int vector_num=ceil(distance/ideal_complement_distance);//NOTE: round等だと距離条件が満たされない場合がある
-  for(int i=1;i<=vector_num;i++){
-    v.push_back((double)i/vector_num*p);
-  }
+  //TODO: 自身の経路が交差していたら短絡させる
+  route.cost+=cost_function(last_point,p);
+  route.length+=distance;
 }
--- src/main/Ant.hpp
+++ src/main/Ant.hpp
@@ -3,13 +3,15 @@
 #include<vector>
 #include<array>
 #include<memory>
+#include<functional>
 using namespace std;
 using ll=long long;
 using v2d=array<double,2>;//TODO: 後でクラスを作り直してリファクタリングしたい
 
 class ACOSteiner;
-
+template<class T> class QuadTree;
 struct SingleRoute;
+enum class JointTarget;
 
 class Ant{
   protected:
@@ -18,8 +20,18 @@ class Ant{
     double total_cost,total_length;//constは付けられないが、生成以降、少なくともpublic関数では定数として扱って良い
     mutable double pheromone_v;
     vector<shared_ptr<SingleRoute>>path;//pair<int,vec>のintで戻るノード数を管理する
-    void properPushBack(vector<v2d> &v,const v2d &e,const v2d &min_point,const v2d &max_point);
-    void addRandVecToOneRoute(const ACOSteiner &w,const Ant *base_ant,int index);
+    void tracePushBack(
+        const int target_index,const v2d &e,
+        const function<double(const v2d&,const v2d&)> &cost_function);
+    void properPushBack(
+        const int target_index,const v2d &e,
+        const v2d &min_point,const v2d &max_point,
+        const function<double(const v2d&,const v2d&)> &cost_function);
+    bool judgeJointToAnotherAnt(const Ant* base_ant);
+    JointTarget archedAdd(const int target_index,const Ant *base_ant,v2d base_random_vec,const ACOSteiner &world);
+    JointTarget addRandVecToOneRoute(const ACOSteiner &w,const Ant *base_ant,int index);
+    void joinToAnotherAnt(const Ant* base_ant);
+    void joinToAnotherOwnRoute(const Ant* base_ant);
     void constructModifiedRoute(const ACOSteiner &w,ll current_time);
     void constructFirstRoute(const ACOSteiner &w);
   public:
