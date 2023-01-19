312085d1c67b512070df4a1f794470e985e03174
tkm3845 <takumi3845@gmail.com>
2023-01-20 03:39:31 +0900

    可視化プログラムの作成

---
+++ analyze/animate.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.animation import ArtistAnimation
+import os
+
+base = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.normpath(os.path.join(base, 'data/data.json'))
+
+data=pd.read_json(data_path)
+uniq=data['points'].drop_duplicates().reset_index(drop=True)
+
+fig = plt.figure(facecolor="w")
+# 0 <=x < 2pi の範囲の点列を作成。
+ims=[]
+for points in uniq:
+    im=[]
+    for single_route in points:
+        tmp=np.array(single_route).T
+        im=im+plt.plot(tmp[0],tmp[1])
+    ims.append(im)
+ani = ArtistAnimation(fig, ims, interval=1000)
+plt.show()
+# mp4ファイルに保存
+#ani.save('animation.mp4', writer='ffmpeg')
+# gifファイルに保存する場合
+# ani.save('animation.gif', writer='pillow')
